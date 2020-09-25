11d2b0c1a9bff273760643e5d3211c8433b8a8ab
baba_s <shotababa1206@gmail.com>
2020-09-25 10:18:16 +0900

    first commit

---
+++ Editor/MissingReferenceUtils.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Kogane
+{
+	public static class MissingReferenceUtils
+	{
+		public static bool HasMissingReference( GameObject gameObject )
+		{
+			var components = gameObject
+					.GetComponents<Component>()
+					.Where( c => c != null )
+				;
+
+			foreach ( var c in components )
+			{
+				var so = new SerializedObject( c );
+				var sp = so.GetIterator();
+
+				while ( sp.NextVisible( true ) )
+				{
+					if ( sp.propertyType != SerializedPropertyType.ObjectReference ) continue;
+					if ( sp.objectReferenceValue != null ) continue;
+					if ( !sp.hasChildren ) continue;
+					var fileId = sp.FindPropertyRelative( "m_FileID" );
+					if ( fileId == null ) continue;
+					if ( fileId.intValue == 0 ) continue;
+
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
